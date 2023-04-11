ed28d7ca070f300957f0ca1372bb6c8f95e84c27
Andrea Zuccherelli <andrea@zuccherelli.net>
2023-04-11 22:33:37 +0200

    working systemd, ssh

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ docker run --rm \
        --name drbl \
        --privileged \
        --net=host \
+       --tmpfs /tmp \
+       --tmpfs /run \
+       -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
        andzuc/debian-drbl:latest
