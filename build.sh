4a3d593c56afb4b491010e8f2bf319a265481afa
Andrea Zuccherelli <andrea@zuccherelli.net>
2023-04-16 20:14:24 +0200

    test

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+export LAST_SERVER_COMMIT=`git ls-remote . "refs/heads/master"`
 tar -C context -cf - . \
     |docker build \
 	    --progress=plain \
+	    --build-arg LAST_SERVER_COMMIT="$LAST_SERVER_COMMIT" \
 	    -t andzuc/debian-drbl \
-	    -
+	    - \
+	    2>&1 \
+    |tee build.log
